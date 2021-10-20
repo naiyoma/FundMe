64d7dcc2209e180e5e2b06d1b209893be58fdbee
Naiyoma <lankas.aurelia@gmail.com>
2021-10-21 00:06:52 +0300

    update contract with ganache address and private key

--- deploy.py
+++ deploy.py
@@ -41,7 +41,7 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 # pdb; pdb.set_trace()
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 chain_id = 1337
-my_address = "0xAfEd3A49a7EA22155F38063b06A17C11Fe2251e3"
+my_address = "0x23cf32dd4Adce7a4021EAFA0f66c1310824eEf6C"
 private_key = os.getenv("PRIVATE_KEY")
 
 # createthecontract in python
@@ -58,3 +58,20 @@ transaction = SimpleStorage.constructor().buildTransaction(
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
 #send the signed transaction
 tx_hash = w3.eth.send_raw_transaction((signed_txn.rawTransaction))
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+print(simple_storage.functions.retrieve().call())
+# create a transaction
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
+)
+# sign a transaction
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+# wait for transaction to finish
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print(simple_storage.functions.retrieve().call())
