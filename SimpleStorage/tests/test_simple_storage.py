6ad1f79fc5dcdc0539a817bd049dcd7e8bb49ffd
Naiyoma <lankas.aurelia@gmail.com>
2021-10-21 22:26:03 +0300

    test storage update function

--- SimpleStorage/tests/test_simple_storage.py
+++ SimpleStorage/tests/test_simple_storage.py
@@ -8,3 +8,12 @@ def test_deploy():
     expected = 0
 
     assert starting_value == expected
+
+def test_updating_storage():
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    #Act
+    expected = 15
+    simple_storage.store(expected, {"from": account})
+    #Assert
+    assert expected == simple_storage.retrieve()
