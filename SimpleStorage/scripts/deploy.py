ed07d4aa436eb75002eced6b9bc74487b8cfd4ca
Naiyoma <lankas.aurelia@gmail.com>
2021-10-21 22:21:33 +0300

    brownie test

--- SimpleStorage/scripts/deploy.py
+++ SimpleStorage/scripts/deploy.py
@@ -1,10 +1,17 @@
 import os
-from brownie import accounts
+from brownie import accounts, config, SimpleStorage
 
 def deploy_simple_storage():
     # account = accounts.load("naiyoma-account")
-    account = accounts.add(os.getenv("PRIVATE_KEY"))
-    import pdb; pdb.set_trace()
+    # account = accounts.add(os.getenv("PRIVATE_KEY"))
+    # account = accounts.add(config["wallets"]["from_key"])
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
 
 def main():
     deploy_simple_storage()
