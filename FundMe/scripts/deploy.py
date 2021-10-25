edb5d591a9a058c2bd12344cb54abbcb2e6dc24b
Naiyoma <lankas.aurelia@gmail.com>
2021-10-25 20:53:30 +0300

    mock aggregator contract

--- FundMe/scripts/deploy.py
+++ FundMe/scripts/deploy.py
@@ -1,9 +1,16 @@
-from brownie import FundMe
+from brownie import FundMe, network, config
 from .help_scripts import get_account
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from":account}, publish_source=True)
+    if network.show_active() != "development":
+        price_feed = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    fund_me = FundMe.deploy(
+        price_feed,
+        {"from": account},
+        publish_source=True)
     # import pdb; pdb.set_trace()
 
 def main():
