72d066e9163af7876a47aa6354ce4d6ce89af228
Naiyoma <lankas.aurelia@gmail.com>
2021-10-25 18:12:43 +0300

    downgrade solidity vesrion

--- FundMe/scripts/deloy.py
+++ FundMe/scripts/deloy.py
@@ -3,7 +3,8 @@ from scripts.helpful.scripts import get_account
 
 def deploy_fund_me():
     account = get_account()
-
+    fund_me = FundMe.deploy({"from":account})
+    import pdb; pdb.set_trace()
 
 def main():
     deploy_fund_me()
