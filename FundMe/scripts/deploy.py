e60cea3657c8bdff0490047f3bce6a0171f46745
Naiyoma <lankas.aurelia@gmail.com>
2021-10-26 21:36:40 +0300

    user should entre based of entrance fee

--- FundMe/scripts/deploy.py
+++ FundMe/scripts/deploy.py
@@ -1,6 +1,7 @@
 from brownie import FundMe, network, config, MockV3Aggregator
 from .help_scripts import get_account
 
+
 def deploy_fund_me():
     account = get_account()
     if network.show_active() != "development":
@@ -10,11 +11,16 @@ def deploy_fund_me():
     else:
         print("The active network is active")
         print("deploy mocks......")
+        mock_aggregator = MockV3Aggregator.deploy(
+            10, 2000000000000000000, {"from": account})
+        price_feed_address = mock_aggregator.address
+        print("Mocks Deployed")
 
     fund_me = FundMe.deploy(
         price_feed,
         {"from": account},
         publish_source=True)
+
 
 def main():
     deploy_fund_me()
---
+++ README.md
@@ -0,0 +1,3 @@
+Users enter the lottery with ETH
+An Admin chooses when the lottery is over(use a function instead)
+The lottery selects a random winner
