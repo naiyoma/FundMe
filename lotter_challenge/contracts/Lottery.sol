e64400b20209082c0f6b55d27059226bbd3de135
Naiyoma <lankas.aurelia@gmail.com>
2021-10-27 23:25:48 +0300

    create a function to facilitate a minimum of 50eth for every player

--- lotter_challenge/contracts/Lottery.sol
+++ lotter_challenge/contracts/Lottery.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable{
     //user enter the competition
     address payable[] public players;
     uint256 public entryFee;
@@ -16,14 +17,16 @@ contract Lottery {
     // 0
     // 1
     // 2
-
+    LOTTERY_STATE public lottery_state;
     //set entry fee
     constructor(address _priceFeedAddress) public {
         entryFee = 50*(10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
-    function enter() public {
-        require(msg.value >= getEntraceFee(), "Not Enough EHT")
+    function enter() public payable{
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntraceFee(), "Not Enough EHT");
         players.push(msg.sender);
     }
     //user to deposit EtH
@@ -34,7 +37,13 @@ contract Lottery {
         return costToEnter;
     }
     //start the lottery
-    // function endLottery() public {}
+    function startLottery() public  onlyOwner{
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
     //end
     // function endLottery() public {}
 }
