34cedb9821df396cd4227c7c190157d90302fade
Naiyoma <lankas.aurelia@gmail.com>
2021-11-07 15:46:32 +0300

    upload to ipfs

--- Mona-NFT/scripts/create_metadata.py
+++ Mona-NFT/scripts/create_metadata.py
@@ -13,9 +13,49 @@ breed_to_image_uri = {
 }
 
 
-# create metadata
 def main():
     advanced_collectible = AdvancedCollectible[-1]
     number_of_advanced_collectibles = advanced_collectible.tokenCounter()
+    print(f"You have created {number_of_advanced_collectibles} collectibles!")
     for token_id in range(number_of_advanced_collectibles):
-        breed = advanced_collectible.tokenIdToBreed(token_id)
+        breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
+        metadata_file_name = (
+            f"./metadata/{network.show_active()}/{token_id}-{breed}.json"
+        )
+        collectible_metadata = metadata_template
+        if Path(metadata_file_name).exists():
+            print(f"{metadata_file_name} already exists! Delete it to overwrite")
+        else:
+            print(f"Creating Metadata file: {metadata_file_name}")
+            collectible_metadata["name"] = breed
+            collectible_metadata["description"] = f"An adorable {breed} pup!"
+            image_path = "./img/" + breed.lower().replace("_", "-") + ".png"
+
+            image_uri = None
+            if os.getenv("UPLOAD_IPFS") == "true":
+                image_uri = upload_to_ipfs(image_path)
+            image_uri = image_uri if image_uri else breed_to_image_uri[breed]
+
+            collectible_metadata["image"] = image_uri
+            with open(metadata_file_name, "w") as file:
+                json.dump(collectible_metadata, file)
+            if os.getenv("UPLOAD_IPFS") == "true":
+                upload_to_ipfs(metadata_file_name)
+
+
+# curl -X POST -F file=@metadata/rinkeby/0-SHIBA_INU.json http://localhost:5001/api/v0/add
+
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        ipfs_url = "http://127.0.0.1:5001"
+        endpoint = "/api/v0/add"
+        response = requests.post(
+            ipfs_url + endpoint, files={"file": image_binary})
+        ipfs_hash = response.json()["Hash"]
+        # "./img/0-PUG.png" -> "0-PUG.png"
+        filename = filepath.split("/")[-1:][0]
+        image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+        print(image_uri)
+        return image_uri
